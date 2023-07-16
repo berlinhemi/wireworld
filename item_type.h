61530038af6aa207ce91020cb33c02733cff7af1
berlinhemi <dennn93@mail.ru>
2023-07-16 09:46:49 -0400

    add enum with item types: head tail ...

---
+++ item_type.h
@@ -0,0 +1,11 @@
+#ifndef MAP_ITEM_H
+#define MAP_ITEM_H
+
+enum E_MAP_ITEMS {
+    E_EMPTY = '0', E_HEAD = '1', E_BODY = '2',
+    E_TAIL = '3', E_CONDUCTOR = '4',
+    /*special labels*/
+     E_BLOCKED = '5', E_HEAD_CANDIDATE = 6}; 
+
+
+#endif
