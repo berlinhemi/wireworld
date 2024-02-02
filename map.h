58c5b26cffee79a5ea04ec8ed83e5e257e410d9c
berlinhemi <dennn93@mail.ru>
2024-02-02 12:50:27 -0500

    misc

--- map.h
+++ map.h
@@ -10,13 +10,13 @@
 
 
 
-void read_map(char*** map, int* height, int* width, const char* file_name);            
+void read_map(char*** map, int* height, int* width, const char* file_name);    
+void print_map(char** map, int height, int width);        
 void clear_map(char*** map, int height, int width);
 
-/*int get_item_position( char** map, int height, int width,  char item_id, Position* pos);*/
 
 E_MAP_ITEM get_item(char** field, Position pos); 
 void put_item(char** map, Position pos,  E_MAP_ITEM item_id);
-void print_map(char** map, int height, int width);
+
 
 #endif
